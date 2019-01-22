9db2ad8a84559d2227eeb18fa86f96e24aa5b927
Vison Li <vison.li@oath.com>
2019-01-22 23:55:28 +0800

    update maven

--- src/main/java/com/yahoo/vison/DynamoDBHelper.java
+++ src/main/java/com/yahoo/vison/DynamoDBHelper.java
@@ -1,6 +1,7 @@
 package com.yahoo.vison;
 
 import com.amazon.dax.client.dynamodbv2.AmazonDaxClient;
+import com.amazon.dax.client.dynamodbv2.AmazonDaxClientBuilder;
 import com.amazon.dax.client.dynamodbv2.ClientConfig;
 import com.amazon.dax.client.dynamodbv2.ClusterDaxClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
@@ -18,12 +19,10 @@ public class DynamoDBHelper {
   }
 
   public static AmazonDynamoDB getDaxClient(String endpoint) {
-    ClientConfig daxConfig = new ClientConfig()
-        .withEndpoints(endpoint)
-        .withCredentialsProvider(AWSCredentialProviderFactory.get());
-
-
-    AmazonDaxClient client = new ClusterDaxClient(daxConfig);
+    AmazonDaxClientBuilder daxClientBuilder = AmazonDaxClientBuilder.standard()
+        .withCredentials(AWSCredentialProviderFactory.get());
+    daxClientBuilder.withRegion(REGION).withEndpointConfiguration(endpoint);
+    AmazonDynamoDB client = daxClientBuilder.build();
     return client;
   }
 }
