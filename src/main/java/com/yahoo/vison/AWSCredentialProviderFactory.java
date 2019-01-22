48226d06ba8ac6673c57533e3f0e97bdcc1be17f
Vison Li <vison.li@oath.com>
2019-01-22 23:23:59 +0800

    Add credential

--- src/main/java/com/yahoo/vison/AWSCredentialProviderFactory.java
+++ src/main/java/com/yahoo/vison/AWSCredentialProviderFactory.java
@@ -1,10 +1,14 @@
 package com.yahoo.vison;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 
 public class AWSCredentialProviderFactory {
   public static AWSCredentialsProvider get() {
+    if (System.getenv("EC2") != null) {
+      return DefaultAWSCredentialsProviderChain.getInstance();
+    }
     return new ProfileCredentialsProvider("federate");
   }
 }
