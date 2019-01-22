a3da8004f75c9a1db8f7ca26a5d5b1599a13ef06
Vison Li <vison.li@oath.com>
2019-01-23 00:05:30 +0800

    multi threading

--- src/main/java/com/yahoo/vison/DaxClientDemo.java
+++ src/main/java/com/yahoo/vison/DaxClientDemo.java
@@ -7,24 +7,46 @@ import com.amazonaws.services.dynamodbv2.model.QueryResult;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DaxClientDemo {
 
   private static String endpoint = "vison-dax2.mkbllw.clustercfg.dax.use2.cache.amazonaws.com:8111";
+  static AmazonDynamoDB client;
 
   public static void main(String[] args) throws Exception {
-    AmazonDynamoDB client;
-
     if (args.length >= 1) {
       client = DynamoDBHelper.getDaxClient(args[0]);
     } else {
       client = DynamoDBHelper.getDynamoDBClient();
     }
 
+    MyRunnable.init();
+    ExecutorService executorService = Executors.newFixedThreadPool(100);
+    for (int i = 0; i < 100; i++) {
+      executorService.execute(new MyRunnable(client, i + 1));
+    }
+
+    // System.out.println(result.getCount());
+  }
+}
+
+class MyRunnable implements Runnable {
+
+  static List<QueryRequest> requests = new ArrayList<QueryRequest>();
+  int index;
+  AmazonDynamoDB client;
+
+  public MyRunnable(AmazonDynamoDB client, int index) {
+    this.index = index;
+    this.client = client;
+  }
+
+  public static void init() {
     HashMap<String, AttributeValue> key = new HashMap<String, AttributeValue>();
     key.put(":a", new AttributeValue().withS("com.yahoo.taipei.bus"));
 
-    List<QueryRequest> requests = new ArrayList<QueryRequest>();
     requests.add(new QueryRequest()
         .withTableName("vison-feed-db")
         .withKeyConditionExpression("appId = :a")
@@ -43,18 +65,15 @@ public class DaxClientDemo {
         .withExpressionAttributeValues(key)
         .withLimit(5000)
     );
+  }
 
-    int iterators = 3;
-    for (int i = 0; i < iterators; i++) {
-      System.out.printf("==== Iterator %d ====\n", i + 1);
-      for (QueryRequest request : requests) {
-        long start = System.nanoTime();
-        QueryResult result = client.query(request);
-        long end = System.nanoTime();
-        System.out.printf("Duration: %.3f ms\n", (end - start) / (1000000.0));
-      }
+  public void run() {
+    System.out.printf("==== Iterator %d ====\n", index);
+    for (QueryRequest request : requests) {
+      long start = System.nanoTime();
+      QueryResult result = client.query(request);
+      long end = System.nanoTime();
+      System.out.printf("Duration: %.3f ms\n", (end - start) / (1000000.0));
     }
-
-    // System.out.println(result.getCount());
   }
 }
