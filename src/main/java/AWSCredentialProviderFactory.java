88e01180bc14fb25318f5d2cb045780c01cd8019
Vison Li <vison.li@oath.com>
2019-01-22 23:09:45 +0800

    build jar:

--- src/main/java/AWSCredentialProviderFactory.java
+++
@@ -1,8 +0,0 @@
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-
-public class AWSCredentialProviderFactory {
-  public static AWSCredentialsProvider get() {
-    return new ProfileCredentialsProvider("federate");
-  }
-}
---
+++ src/main/java/com/yahoo/vison/AWSCredentialProviderFactory.java
@@ -0,0 +1,10 @@
+package com.yahoo.vison;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+
+public class AWSCredentialProviderFactory {
+  public static AWSCredentialsProvider get() {
+    return new ProfileCredentialsProvider("federate");
+  }
+}
